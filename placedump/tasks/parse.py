2a74fb2bae3b859dee0842077cf493b002410a16
erin <nepeat@gmail.com>
2022-04-02 21:35:23 +0000

    parse simplified schema for data

--- placedump/tasks/parse.py
+++ placedump/tasks/parse.py
@@ -6,37 +6,39 @@ from placedump.tasks.pixels import download_url
 
 
 @app.task()
-def parse_message(message: str):
+def parse_message(message: str, canvas_id: int = None):
     try:
         payload = json.loads(message)
     except json.JSONDecodeError:
         return
 
-    # Get board ID from subscription number.
-    board = min(
-        int(payload.get("id", 2)) - 2,  # Default to root board if we have no ID.
-        0,  # Default to first board if we something invalid.
-    )
+    # Get canvas ID from subscription number.
+    if not canvas_id:
+        canvas_id = min(
+            int(payload.get("id", 2)) - 2,  # Default to root board if we have no ID.
+            0,  # Default to first board if we something invalid.
+        )
 
     # Attempt to handle based on message type.
     with ctx_redis() as redis:
         try:
-            message_type = payload["payload"]["data"]["subscribe"]["data"]["__typename"]
+            message_type = payload["subscribe"]["data"]["__typename"]
 
             if message_type == "ConfigurationMessageData":
                 highest_index = 0
-                for canvas in payload["payload"]["data"]["subscribe"]["data"][
-                    "canvasConfigurations"
-                ]:
+                for canvas in payload["subscribe"]["data"]["canvasConfigurations"]:
                     highest_index = max(canvas["index"], highest_index)
                 redis.hset("place:meta", "index", highest_index)
         except KeyError:
             pass
 
     try:
-        url = payload["payload"]["data"]["subscribe"]["data"]["name"]
+        url = payload["subscribe"]["data"]["name"]
     except KeyError:
-        print(payload)
-        return
+        try:
+            url = payload["payload"]["data"]["subscribe"]["data"]["name"]
+        except KeyError:
+            print(payload)
+            return
 
-    download_url.delay(board, url)
+    download_url.delay(canvas_id, url)
